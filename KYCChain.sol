a9e066377e5e6ddc4aa999512488c110e2df5844
Satish-Nagar <164318446+Satish-Nagar@users.noreply.github.com>
2025-07-18 22:07:02 +0530

    Add files via upload

---
+++ KYCChain.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+contract KYCChain {
+    address public admin;
+    mapping(address => bool) public verifiedUsers;
+    mapping(address => string) private userMetadata;
+
+    event UserVerified(address indexed user, string metadata);
+    event UserRevoked(address indexed user);
+
+    constructor() {
+        admin = msg.sender;
+    }
+
+    modifier onlyAdmin() {
+        require(msg.sender == admin, "Only admin can perform this action");
+        _;
+    }
+
+    function verifyUser(address user, string calldata metadata) external onlyAdmin {
+        verifiedUsers[user] = true;
+        userMetadata[user] = metadata;
+        emit UserVerified(user, metadata);
+    }
+
+    function revokeUser(address user) external onlyAdmin {
+        verifiedUsers[user] = false;
+        emit UserRevoked(user);
+    }
+
+    function isUserVerified(address user) external view returns (bool) {
+        return verifiedUsers[user];
+    }
+
+    function getUserMetadata(address user) external view returns (string memory) {
+        require(verifiedUsers[user], "User not verified");
+        return userMetadata[user];
+    }
+}
